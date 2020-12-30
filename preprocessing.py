b36dc58093e2cf6f83ff5d21470a650882cbfaf4
d.f.prijs <d.f.prijs@students.uu.nl>
2020-12-30 21:41:10 +0100

    Dataframes get merged on key columns

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import glob
 
-# Allows for printing all columns; debug utility
+# Allows for printing all columns and increases width; debug utility
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 
 # Select only the data files in the data folder
 data_files = glob.glob('data/*data.csv')
 
-dfs = []
+# We create the dfs and merge them on the keys TIME and METROREG
+dfs = None
 for file in data_files:
-    dfs.append(pd.read_csv(file, encoding="ISO-8859-1"))
+    current_df = pd.read_csv(file, encoding="ISO-8859-1")
+    if dfs is not None:
+        dfs = pd.merge(dfs, current_df, on=["TIME", "METROREG"])
+    else:
+        dfs = current_df
 
-print(dfs[0])
-print(dfs[1])
+print(dfs)
