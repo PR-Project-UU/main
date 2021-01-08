c5cea2e5ca5bcae790948ae0d7b12ac424f6fd7e
d.f.prijs <d.f.prijs@students.uu.nl>
2021-01-08 15:34:28 +0200

    Coords are now added to the aggregate table, fixed code layout, etc

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import glob
+import numpy as np
+import geopy
+from geopy.geocoders import Nominatim
+from functools import partial
+
 
 # Allows for printing all columns and increases width; debug utility
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 
 # Select only the data files in the data folder
-#data_files = glob.glob('data/*data.csv')
 data_files = sorted(glob.glob('data/*data.csv'))
 
 # Create the dfs and merge them on the keys TIME and METROREG
@@ -31,52 +35,66 @@ filtered_data = merged_data.rename(columns=renames).drop(columns=droppes)
 
 # print(filtered_data)
 
-# The following allows to see all values of METROREG without doublets, that's how I retrieved the rows which we do not want
-#sorted(set(list(filtered_data.METROREG)))
+# The following allows to see all values of METROREG without doublets, that's how I retrieved the rows which we
+# do not want
+# sorted(set(list(filtered_data.METROREG)))
 
-non_metro=['Non-metropolitan regions in Austria','Non-metropolitan regions in Belgium','Non-metropolitan regions in Bulgaria','Non-metropolitan regions in Croatia',
- 'Non-metropolitan regions in Czech Republic','Non-metropolitan regions in Denmark','Non-metropolitan regions in Estonia','Non-metropolitan regions in Finland',
- 'Non-metropolitan regions in Germany','Non-metropolitan regions in Greece','Non-metropolitan regions in Hungary','Non-metropolitan regions in Ireland',
- 'Non-metropolitan regions in Italy','Non-metropolitan regions in Latvia','Non-metropolitan regions in Lithuania','Non-metropolitan regions in Malta',
- 'Non-metropolitan regions in Netherlands','Non-metropolitan regions in North Macedonia','Non-metropolitan regions in Portugal','Non-metropolitan regions in Romania',
- 'Non-metropolitan regions in Serbia','Non-metropolitan regions in Slovakia','Non-metropolitan regions in Slovenia','Non-metropolitan regions in Spain',
- 'Non-metropolitan regions in Sweden','Non-metropolitan regions in United Kingdom']
+non_metro = ['Non-metropolitan regions in Austria', 'Non-metropolitan regions in Belgium',
+             'Non-metropolitan regions in Bulgaria', 'Non-metropolitan regions in Croatia',
+             'Non-metropolitan regions in Czech Republic', 'Non-metropolitan regions in Denmark',
+             'Non-metropolitan regions in Estonia', 'Non-metropolitan regions in Finland',
+             'Non-metropolitan regions in Germany', 'Non-metropolitan regions in Greece',
+             'Non-metropolitan regions in Hungary', 'Non-metropolitan regions in Ireland',
+             'Non-metropolitan regions in Italy', 'Non-metropolitan regions in Latvia',
+             'Non-metropolitan regions in Lithuania', 'Non-metropolitan regions in Malta',
+             'Non-metropolitan regions in Netherlands', 'Non-metropolitan regions in North Macedonia',
+             'Non-metropolitan regions in Portugal', 'Non-metropolitan regions in Romania',
+             'Non-metropolitan regions in Serbia', 'Non-metropolitan regions in Slovakia',
+             'Non-metropolitan regions in Slovenia', 'Non-metropolitan regions in Spain',
+             'Non-metropolitan regions in Sweden', 'Non-metropolitan regions in United Kingdom']
 
-countries = countries = ["United Kingdom","West Midlands urban area","North Macedonia","Austria","Belgium","Bulgaria","Croatia",
-                         "Cyprus","Czechia","Denmark","Estonia","Finland","France","Germany","Germany (until 1990 former territory of the FRG)","Greece",
-                         "Hungary","Ireland","Italy","Latvia","Lithuania","Luxembourg","Malta","Netherlands","Poland","Portugal","Romania","Serbia",
-                         "Slovakia","Slovenia","Spain","Sweden"]
-countries.extend(non_metro) #merges the two lists above
+countries = ["United Kingdom", "West Midlands urban area", "North Macedonia", "Austria", "Belgium", "Bulgaria",
+             "Croatia", "Cyprus", "Czechia", "Denmark", "Estonia", "Finland", "France", "Germany",
+             "Germany (until 1990 former territory of the FRG)", "Greece", "Hungary", "Ireland", "Italy",
+             "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania",
+             "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden"]
 
-#Removing countries and zones we do not want
-filtered_data2=filtered_data
+# Merges the two lists above
+countries.extend(non_metro)
+
+# Removing countries and zones we do not want
+filtered_data2 = filtered_data
 for i in countries:
-    filtered_data2=filtered_data2[filtered_data2.METROREG!=i]
+    filtered_data2 = filtered_data2[filtered_data2.METROREG != i]
 
-#Preprocessing on the population column
-filtered_data2.population = filtered_data2.population.str.replace(r':','0') #replacing missing values (noted ':') by 0
-filtered_data2.population = filtered_data2['population'].str.replace(r',', '') #removing commas 
-filtered_data2.population = filtered_data2['population'].astype(int) #changing column type
+# Preprocessing on the population column
+filtered_data2.population = filtered_data2.population.str.replace(r':', '0')  # replacing NAs (noted ':') by 0
+filtered_data2.population = filtered_data2['population'].str.replace(r',', '')  # removing commas
+filtered_data2.population = filtered_data2['population'].astype(int)  # changing column type
 
-#Preprocessing on the GDP column
-filtered_data2.gdp = filtered_data2.gdp.str.replace(r':','0') #replacing missing values (noted ':') by 0
-filtered_data2.gdp = filtered_data2['gdp'].str.replace(r',', '') #removing commas 
-filtered_data2.gdp = filtered_data2['gdp'].astype(float) #changing column type
+# Preprocessing on the GDP column
+filtered_data2.gdp = filtered_data2.gdp.str.replace(r':', '0')  # replacing missing values (noted ':') by 0
+filtered_data2.gdp = filtered_data2['gdp'].str.replace(r',', '')  # removing commas
+filtered_data2.gdp = filtered_data2['gdp'].astype(float)  # changing column type
 
-#employed_persons column
-filtered_data2.employed_persons = filtered_data2.employed_persons.astype(str).str.replace(r':','0')
+# Employed_persons column
+filtered_data2.employed_persons = filtered_data2.employed_persons.astype(str).str.replace(r':', '0')
 filtered_data2.employed_persons = filtered_data2['employed_persons'].str.replace(r',', '')
 filtered_data2.employed_persons = filtered_data2['employed_persons'].astype(float)
 
-#OPTIMAL VALUES TO BE DETERMINED
-#As of now, we look at cities with minimum thresholds of population>500000 and GDP>40000, could be changed if we need more/less data
-filtered_data3 = filtered_data2[(filtered_data2['TIME']>=2000) & (filtered_data2['population']>500000) & (filtered_data2['gdp']>40000)]
+# OPTIMAL VALUES TO BE DETERMINED
+# As of now, we look at cities with minimum thresholds of population>500000 and GDP>40000,
+# could be changed if we need more/less data
+filtered_data3 = filtered_data2[(filtered_data2['TIME'] >= 2000) &
+                                (filtered_data2['population'] > 500000) &
+                                (filtered_data2['gdp'] > 40000)]
 
-#Geolocation part
+###### Geolocation part
 
 """
-To bridge the images and the socio economic data, we can use the geopy package to retrieve the latitude & longitude of the cities 
-in our data set. From there, we need to figure out a way to access the satellite images w/latitude & longitude
+To bridge the images and the socioeconomic data, we can use the geopy package to retrieve the latitude & 
+longitude of the cities in our data set. From there, we need to figure out a way to access the satellite 
+images w/latitude & longitude
 """
 
 # # Directly covered from https://developers.google.com/earth-engine/guides/landsat
@@ -93,47 +111,47 @@ in our data set. From there, we need to figure out a way to access the satellite
 # toa = ee.Algorithms.Landsat.TOA(raw);
 # Map.addLayer(toa, {bands: ['B4', 'B3', 'B2'], max: 0.2}, 'toa reflectance');
 
-#Needed dependency:
+######
 
-#pip install geopy
+# Creating a function to retrieve latitude & longitude from a string
+geo_locator = Nominatim(user_agent="Master-Student")
 
-import geopy
-from geopy.geocoders import Nominatim
-from functools import partial
 
-#Creating a function to retrieve latitude & longitude from a string
-
-geolocator = Nominatim(user_agent="Master-Student")
-
+# returns a tuple in the form (latitude,longitude)
 def get_coord(k):
     assert type(k) == str
-    return (geolocator.geocode(k).latitude,geolocator.geocode(k).longitude) # returns a tuple in the form (latitude,longitude)
+    return geo_locator.geocode(k).latitude, geo_locator.geocode(k).longitude
 
-#Next, we need to add an extra column in which we store the latitudes & longitudes of the corresponding rows
 
-latlong = [0 for i in range(filtered_data3.shape[0])] #list full of zeros, could improve the code w/numpy later
+# Next, we need to add an extra column in which we store the latitudes & longitudes of the corresponding rows
+filtered_data3['latitude'] = filtered_data3['longitude'] = np.zeros(len(filtered_data3))
 
-#Adding the extra  empty columns:
-filtered_data3['latitude'] = latlong
-filtered_data3['longitude'] = latlong
+# Creating a list with cities and their coords
+coords_list = []
+for city in sorted(list(set(list(filtered_data3.METROREG))))[:5]:  # [:5] is voor debug, weghalen later
+    try:
+        city_coords = get_coord(city)
+        coords_list.append([city, city_coords])
+    except:
+        print("Couldn't get coords of:", city)
 
-d={} #Creating a dictionnary with cities as keys and position as values
-for i in sorted(set(list(filtered_data3.METROREG))):
-    d[i]=get_coord(i)
 
-#Adding corresponding positions to cities
-#Problem : was only able to add the coordinates as strings and not tuples :(, this needs to be changed otherwise it's gonna slow computation speed
-for i in d.keys():
-    for idx, row in filtered_data3.iterrows():
-        if  filtered_data3.loc[idx,'METROREG'] == i: #The following fixes the type problem previously encountered
-            filtered_data3.loc[idx,'latitude'] = list(d.values())[i][0] #accesses the latitude
-            filtered_data3.loc[idx,'longitude'] = list(d.values())[i][1] #accesses the longitude
-            
-#We need to delete the cities for which we do not have enough data for, here I set the the minimum number of instances to be at least 10 years of data per city
+# Adding corresponding positions to cities
+for coords_city in coords_list:
+    city = coords_city[0]
+    coords = coords_city [1]
+    filtered_data3.loc[filtered_data3['METROREG'] == city, 'latitude'] = coords[0]
+    filtered_data3.loc[filtered_data3['METROREG'] == city, 'longitude'] = coords[1]
 
-d2=dict(filtered_data3['METROREG'].value_counts()) #This dictionnary stores the number of times each city appears in the data
+# We need to delete the cities for which we do not have enough data for,
+# here I set the the minimum number of instances to be at least 10 years of data per city
+# This dictionary stores the number of times each city appears in the data
+d2 = dict(filtered_data3['METROREG'].value_counts())
 
-for i in d2.keys(): #This for loop removes all rows for cities on which we do not have enough data
-    if d2[i]<10: #10 years at least
-        filtered_data3=filtered_data3[filtered_data3.METROREG!=i]
-#yields filterred_data3 in the shape (614, 7)
+# This for loop removes all rows for cities on which we do not have enough data
+for i in d2.keys():
+    if d2[i] < 10:  # 10 years at least
+        # Yields filtered_data3 in the shape (614, 7)
+        filtered_data3 = filtered_data3[filtered_data3.METROREG != i]
+
+# Print(filtered_data3)
