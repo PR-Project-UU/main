3b6e990cb59895080b65e8386d7c8aea7245b710
Galactic-Cat <Galactic-Cat@outlook.com>
2021-01-13 00:29:08 +0100

    Started work on main function

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from arguments import args
+from logging import getLogger
 
-def import_test():
-    import satellite
-    print('imported satellite')
+def not_implemented():
+    log = getLogger()
+    log.info('This mode (%s) is yet to be implemented', args.mode[0])
 
-print('hello world')
+def generate():
+    '''Runs the generation cycle'''
+    row = 0
+
+mode_table = {
+    'generate': not_implemented, #generate,
+    'preprocess': not_implemented,
+    'predict': not_implemented,
+    'train': not_implemented,
+}
+
+mode_table[args.mode[0]]()
